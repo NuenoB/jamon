b79c644eb79bb9d2cc1c2b0baeb05366d5d27921
Enzo <enzo.cerfogli@gmail.com>
2018-04-25 22:07:46 -0300

    added background handeler and grid

--- Control.py
+++ Control.py
@@ -1,39 +1,13 @@
 import pygame
 import pygame.locals
+from dcc_graphics import proto_graphics
 
 if __name__ == "__main__":
-    screen = pygame.display.set_mode((424, 320))
-
-    MAP_TILE_WIDTH = 24
-    MAP_TILE_HEIGHT = 16
-    MAP_CACHE = {
-        'ground.png': load_tile_table('pgame-tiled-tileset.png', MAP_TILE_WIDTH,
-                                      MAP_TILE_HEIGHT),
-    }
-
-    level = Level()
-    level.load_file('level.map')
-
-    clock = pygame.time.Clock()
-
-    background, overlay_dict = level.render()
-    overlays = pygame.sprite.RenderUpdates()
-    for (x, y), image in overlay_dict.iteritems():
-        overlay = pygame.sprite.Sprite(overlays)
-        overlay.image = image
-        overlay.rect = image.get_rect().move(x * 24, y * 16 - 16)
-    screen.blit(background, (0, 0))
-    overlays.draw(screen)
-    pygame.display.flip()
-    game_over = False
-
+    game_over=False
+    pg=proto_graphics()
+    pg.load_background("img/map/[FE8] Indoor Tutorial.png",20,25)
     while not game_over:
-
-        # XXX draw all the objects here
-
-        overlays.draw(screen)
-        pygame.display.flip()
-        clock.tick(15)
+        pg.draw()
         for event in pygame.event.get():
             if event.type == pygame.locals.QUIT:
                 game_over = True
