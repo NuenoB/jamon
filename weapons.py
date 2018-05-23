e388d669e466c2bdecb332dda837212f6940cd42
Nueno <enzo.cerfogli@gmail.com>
2018-05-23 15:33:45 -0400

    buenas those-things.

---
+++ weapons.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed may 16 15:29:28 2018
+
+@author: enzoc
+"""
+import pygame
+
+
+class weapons():
+    def __init__(self):
+    	self.name="Generic name"
+    	self.damage=1
+    	self.type=1
